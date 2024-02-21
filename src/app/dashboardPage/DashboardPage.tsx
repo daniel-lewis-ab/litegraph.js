f0a902788e01bbf49b08847fdec8b5b4440ef8db
Mateusz Blyszcz <mat@superformula.com>
2024-02-21 18:13:48 +0100

    add font-awesome free icons

--- src/app/dashboardPage/DashboardPage.tsx
+++ src/app/dashboardPage/DashboardPage.tsx
@@ -1,1 +1,9 @@
-export const DashboardPage = () => <h1 className="text-3xl font-bold italic underline ml-4 text-error">Dashboard</h1>;
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const DashboardPage = () => (
+  <div>
+    <h1 className="text-3xl font-bold italic underline ml-4 text-error">Dashboard</h1>
+    <FontAwesomeIcon icon={faCoffee} />
+  </div>
+);
