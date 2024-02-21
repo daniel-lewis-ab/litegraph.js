55a1568a3debfa23f263db5133d5a25665e0d42c
Mateusz Blyszcz <mat@superformula.com>
2024-02-21 15:09:21 +0100

    add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#A08DEF',
+        secondary: '#4C4B7F',
+        tertiary: '#2D2E4E',
+        success: '#4AB637',
+        warning: '#D5A84C',
+        error: '#CF512F',
+        surface: '#080812',
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
