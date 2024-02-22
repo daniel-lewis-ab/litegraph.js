3e498358a2a107be32a29cd9faf8013b76ccb5e9
Mateusz Blyszcz <mat@superformula.com>
2024-02-22 20:16:24 +0100

    fix issue with dynamic classes

--- src/shared/components/icon/Icon.tsx
+++ src/shared/components/icon/Icon.tsx
@@ -1,5 +1,4 @@
 import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
-import clsx from 'clsx';
 
 type IconProps = {
   size: number;
@@ -8,11 +7,10 @@ type IconProps = {
 export const Icon = ({ size, width, height, ...props }: IconProps) => (
   <FontAwesomeIcon
     // We need to pass classes because font awesome is adding .svg-inline--fa class on the svg element and it has higher specificity than svg height/width
-    className={clsx(
-      size !== undefined && `h-[${size}px] w-[${size}px]`,
-      height !== undefined && `h-[${height}px]`,
-      width !== undefined && `h-[${width}px]`,
-    )}
+    style={{
+      height: height ? `${height}px` : size ? `${size}px` : undefined,
+      width: width ? `${width}px` : size ? `${size}px` : undefined,
+    }}
     height={height}
     width={width}
     {...props}
