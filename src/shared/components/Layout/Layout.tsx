1ac80879d67d4410c654b2973de37a321399ccd7
Mateusz Blyszcz <mat@superformula.com>
2024-02-28 11:51:19 +0100

    update Layout

--- src/shared/components/Layout/Layout.tsx
+++ src/shared/components/Layout/Layout.tsx
@@ -1,16 +1,15 @@
 import { Icon } from '@/shared/components/icon/Icon';
 import { Logo } from '../icons/Logo';
 import { LayoutProps } from './Layout.types';
-import {
-  faCircleUser,
-  faDatabase,
-  faGear,
-  faRocket,
-  faCodeBranch,
-  faHeartPulse,
-} from '@awesome.me/kit-b6cda292ae/icons/classic/light';
 import { useAuth } from '@/hooks/useAuth/useAuth';
 import { useNavigate } from 'react-router-dom';
+import {
+  faCodeBranch,
+  faRocket,
+  faDatabase,
+  faWavePulse,
+  faCircleUser,
+} from '@awesome.me/kit-b6cda292ae/icons/sharp/light';
 
 export const Layout = ({ children }: LayoutProps) => {
   const navigate = useNavigate();
@@ -23,24 +22,23 @@ export const Layout = ({ children }: LayoutProps) => {
 
   return (
     <div className="flex min-h-screen flex-row">
-      <div className="flex w-[64px] flex-col items-center justify-between border-r border-r-[#626289]">
+      <div className="flex w-[64px] flex-col items-center justify-between border-r border-r-[#313155]">
         <Logo className="mt-2" />
         <div className="flex flex-col *:my-[20px]">
-          <Icon size={30} icon={faRocket} color="#9F9FC5" />
-          <Icon size={30} icon={faCodeBranch} color="#9F9FC5" />
-          <Icon size={30} icon={faDatabase} color="#9F9FC5" />
-          <Icon size={30} icon={faHeartPulse} color="#9F9FC5" />
+          <Icon size={30} icon={faCodeBranch} className="text-secondary" />
+          <Icon size={30} icon={faRocket} className="text-secondary" />
+          <Icon size={30} icon={faDatabase} className="text-secondary" />
+          <Icon size={30} icon={faWavePulse} className="text-secondary" />
         </div>
         <div className="h-[64px]"></div>
       </div>
       <div className="flex flex-grow flex-col">
-        <header className="flex h-[64px] flex-row items-center justify-between border-b border-[#626289] px-6">
+        <header className="flex h-[64px] flex-row items-center justify-between border-b border-[#313155] px-6">
           <div className="flex flex-row items-center">
             <p className="pl-3 text-2xl font-medium">Workflows</p>
           </div>
           <div className="flex flex-row *:mx-4">
-            <Icon size={30} icon={faGear} color="#9F9FC5" />
-            <Icon size={30} icon={faCircleUser} color="#9F9FC5" />
+            <Icon size={30} icon={faCircleUser} className="text-secondary" />
             <button onClick={handleLogout}>Logout</button>
           </div>
         </header>
