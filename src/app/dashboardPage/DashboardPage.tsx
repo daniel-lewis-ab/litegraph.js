2857c8e99b440238b3f372d13028b3eb57f555ce
Mateusz Blyszcz <mat@superformula.com>
2024-02-28 11:56:14 +0100

    update Dashbaord

--- src/app/dashboardPage/DashboardPage.tsx
+++ src/app/dashboardPage/DashboardPage.tsx
@@ -1,34 +1,28 @@
-import { useAuth } from '@/hooks/useAuth/useAuth';
 import { Icon } from '@/shared/components/icon/Icon';
 import { faPlus } from '@awesome.me/kit-b6cda292ae/icons/sharp/solid';
 
 export const DashboardPage = () => {
-  const { currentUser } = useAuth();
-
   return (
     <div className="flex w-full items-center justify-center">
-      <div className="w-[40%] *:mb-5">
-        <button className="flex w-full flex-row items-center rounded-2xl border border-[#313155] p-4">
-          <Icon size={20} icon={faPlus} className="rounded-full bg-surface-100 p-[20px]" />
+      <div className="w-[34%] *:mb-3">
+        <button className="flex w-full flex-row items-center rounded-2xl border border-[#313155] px-6 py-4">
+          <Icon size={20} icon={faPlus} className="rounded-full bg-surface-100 p-[12px]" />
           <div className="flex flex-col items-start justify-start pl-2.5">
-            <p className="font-bold">New Workflow ${currentUser?.displayName}</p>
-            {/* @TODO: Change to inter */}
+            <p className="font-bold">New Workflow</p>
             <p className="text-[#9F9FC5]">Start with an empty project</p>
           </div>
         </button>
-        <button className="flex w-full flex-row items-center rounded-2xl border border-[#313155] p-4">
-          <Icon size={20} icon={faPlus} className="rounded-full bg-surface-100 p-[20px]" />
+        <button className="flex w-full flex-row items-center rounded-2xl border border-[#313155] px-6 py-4">
+          <Icon size={20} icon={faPlus} className="rounded-full bg-surface-100 p-[12px]" />
           <div className="flex flex-col items-start justify-start pl-2.5">
             <p className="font-bold">From Template</p>
-            {/* @TODO: Change to inter */}
             <p className="text-[#9F9FC5]">Explore powerful templates</p>
           </div>
         </button>
-        <button className="!mb-0 flex w-full flex-row items-center rounded-2xl border border-[#313155] p-4">
-          <Icon size={20} icon={faPlus} className="rounded-full bg-surface-100 p-[20px]" />
+        <button className="!mb-0 flex w-full flex-row items-center rounded-2xl border border-[#313155] px-6 py-4">
+          <Icon size={20} icon={faPlus} className="rounded-full bg-surface-100 p-[12px]" />
           <div className="flex flex-col items-start justify-start pl-2.5">
             <p className="font-bold">Import</p>
-            {/* @TODO: Change to inter */}
             <p className="text-[#9F9FC5]">Upload JSON</p>
           </div>
         </button>
