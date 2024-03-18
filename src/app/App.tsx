9b78d8b414cc6f6cef5aedf53ba27e785f633490
Mateusz Blyszcz <mat@superformula.com>
2024-03-18 17:17:36 +0100

    remove redundant router provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,7 +11,6 @@ function App() {
     <AuthContextProvider>
       <QueryClientProvider>
         <WebSocketProvider>
-          <RouterProvider router={router} />
           {createPortal(
             <Toaster
               position="bottom-right"
