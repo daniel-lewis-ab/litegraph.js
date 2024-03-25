70cc3c619140be469052fbecee3abbbe45fd4ffe
Scott Baggett <scott.baggett@plailabs.com>
2024-03-25 14:13:55 +0000

    Button Updates - Levels, Colors, Tweaks from DS

--- src/shared/components/button/Button.tsx
+++ src/shared/components/button/Button.tsx
@@ -1,9 +1,9 @@
 import clsx from 'clsx';
 import { ReactNode } from 'react';
+import { Link, To } from 'react-router-dom';
 import { Icon, IconProps } from '../icon/Icon';
 import { LoaderIcon } from '../loaderIcon/LoaderIcon';
 import './Button.scss';
-import { Link, To } from 'react-router-dom';
 
 type LinkProps = {
   to?: To;
@@ -17,6 +17,7 @@ type ButtonProps = LinkProps & {
   color?: 'primary' | 'secondary' | 'success' | 'error' | 'warning';
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
   disabled?: boolean;
+  level?: number;
   type?: 'button' | 'submit';
   isLoading?: boolean;
   leftIcon?: IconProps['icon'];
@@ -41,6 +42,7 @@ export const Button = ({
   type = 'button',
   isLoading,
   asLink,
+  level,
   to,
 }: ButtonProps) => {
   const ButtonOrLink = asLink && to ? Link : 'button';
@@ -54,13 +56,14 @@ export const Button = ({
         'rounded-lg font-medium',
         size === 'xs' && 'space-x-1 rounded-md px-2 py-1 text-xs',
         size === 'sm' && 'space-x-1 px-3 py-1.5 text-sm',
+        size === 'md' && 'space-x-2 px-3 py-2 text-sm',
         size === 'xl' && 'space-x-2 rounded-xl px-4 py-[13px] text-xl',
         size === 'lg' && 'space-x-2 rounded-xl px-4 py-[10.5px] text-lg',
-        size === 'md' && 'space-x-2 rounded-lg px-4 py-2',
 
         variant && `button--${variant}`,
         color && `button--color-${color}`,
         disabled && 'button--disabled',
+        level && `button--level-${level}`,
         className,
       )}
       onClick={onClick}
--- src/shared/components/switch/Switch.tsx
+++ src/shared/components/switch/Switch.tsx
@@ -8,7 +8,7 @@ const Switch = forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={clsx(
-      'focus-visible:ring-ring focus-visible:ring-offset-background peer inline-flex h-8 w-14 shrink-0 cursor-pointer items-center rounded-full border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-success-10 data-[state=unchecked]:bg-warning-9',
+      'focus-visible:ring-ring focus-visible:ring-offset-background peer inline-flex h-8 w-14 shrink-0 cursor-pointer items-center rounded-full border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-success-9 data-[state=unchecked]:bg-warning-9',
       className,
     )}
     {...props}
