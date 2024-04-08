260685a1539dd0e6786949879cd8a26b86abcb88
Mateusz Blyszcz <mat@superformula.com>
2024-04-08 17:17:50 +0200

    add env type definitions

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,17 @@
-/// <reference types="vite/client" />
+/// <reference types: string;
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  VITE_FIREBASE_API_KEY: string;
+  VITE_FIREBASE_AUTH_DOMAIN: string;
+  VITE_API_URL: string;
+  VITE_WEBSOCKET_URL: string;
+  VITE_IFRAME_URL: string;
+  VITE_IMAGE_SERVICE_URL: string;
+  VITE_SHOW_IFRAME_LOGS: string;
+  VITE_SHOW_WEBSOCKET_LOGS: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
