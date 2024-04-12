738d9d6a2366831db7abcb83e85ac66a8c68fd87
Scott Baggett <hello@scottbaggett.com>
2024-04-11 21:35:39 -0400

    fix: center align error text

--- src/shared/components/pageErrorTemplate/PageErrorTemplate.tsx
+++ src/shared/components/pageErrorTemplate/PageErrorTemplate.tsx
@@ -1,8 +1,8 @@
 import { constants } from '@/contants';
-import { Button } from '../button/Button';
-import { useNavigate } from 'react-router-dom';
 import { routes } from '@/routes/routes';
 import clsx from 'clsx';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../button/Button';
 
 type PageErrorTemplateProps = {
   variant: '404' | 'down';
@@ -14,7 +14,7 @@ export const PageErrorTemplate = ({ variant, className, inApp = true }: PageErro
   const navigate = useNavigate();
 
   return (
-    <div className={clsx('flex h-full w-full flex-col items-center justify-center', className)}>
+    <div className={clsx('flex h-full w-full flex-col items-center justify-center px-8 text-center', className)}>
       {variant === '404' ? <p className="mb-2 font-mono text-xs">404</p> : null}
       <h1 className="text-5xl">{variant === '404' ? 'Page not found' : "Salt is down, we're on it"}</h1>
       {variant === 'down' ? <p className="mt-2">Got questions? Come find us on Discord.</p> : null}
